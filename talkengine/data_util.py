bd4d55948ff8ac8417916b92db97800a4db2b700
MJ-Jang <xkxpa@korea.ac.kr>
2019-12-03 14:07:49 +0900

    update model DB connection logic

--- talkengine/data_util.py
+++ talkengine/data_util.py
@@ -1,36 +1,39 @@
 import pandas as pd
 import numpy as np
-import pymysql
+
+import importlib
+
+import sys
+import os
+
+root_path = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+sys.path.append(root_path + '/backend')
+
+mod = importlib.import_module('model.scenario_dao')
+ScenarioDao = mod.ScenarioDao
 
 
 class DataController:
-    def __init__(self, config, inferencer=None):
-        self.conn = pymysql.connect(host=config.host,
-                                    port=config.port,
-                                    user=config.user,
-                                    passwd=config.passwd,
-                                    db=config.db,
-                                    charset=config.charset)
+    def __init__(self, inferencer=None):
         self.inferencer = inferencer
         self.init_setup()
 
     def init_setup(self):
-        res_class, self.responses = self._get_response()
-        query_class, querys = self._get_query()
-
+        (query_class, querys), (res_class, self.responses) = self._get_query_response()
         self.query_embedding_dict = construct_ques_embed_dict(querys, query_class, self.inferencer)
         self.response_cluster_dict = construct_res_cluster_dict(self.responses, res_class)
         return self.query_embedding_dict, self.response_cluster_dict
 
     def update(self):
-        query_embedding_dict = self._update_query()
-        response_cluster_dict = self._update_response()
+        query_embedding_dict, response_cluster_dict = self._update_query_response()
         return query_embedding_dict, response_cluster_dict
 
-    def _update_query(self):
-        query_class, querys = self._get_query()
+    def _update_query_response(self):
+        (query_class, querys), (response_class, responses) = self._get_query_response()
 
-        pre_query_set = [(i, t) for i, t in zip(self.query_embedding_dict['class'], self.query_embedding_dict['sentences'])]
+        # update query
+        pre_query_set = [(i, t) for i, t in
+                         zip(self.query_embedding_dict['class'], self.query_embedding_dict['sentences'])]
         new_query_set = [(i, t) for i, t in zip(query_class, querys)]
 
         new_query = list(set(new_query_set).difference(pre_query_set))
@@ -50,57 +53,34 @@ class DataController:
             query_embed_dict = {}
             for k in self.query_embedding_dict.keys():
                 if self.query_embedding_dict[k].__class__ == np.ndarray:
-                    query_embed_dict[k] = [d[k] for d in np.concatenate([self.query_embedding_dict, new_query_embedding_dict])]
+                    query_embed_dict[k] = [d[k] for d in
+                                           np.concatenate([self.query_embedding_dict, new_query_embedding_dict])]
                 else:
                     query_embed_dict[k] = [d[k] for d in [self.query_embedding_dict, new_query_embedding_dict]]
             self.query_embedding_dict = query_embed_dict
-        return self.query_embedding_dict
 
-    def _update_response(self):
-        response_class, response = self._get_response()
-        if response == self.responses:
-            return self.response_cluster_dict
+        # update response
+        if responses == self.responses:
+            response_cluster_dict = self.response_cluster_dict
         else:
-            response_cluster_dict = construct_res_cluster_dict(response, response_class)
-            self.responses = response
-        return response_cluster_dict
+            response_cluster_dict = construct_res_cluster_dict(responses, response_class)
+            self.responses = responses
 
-    def _get_response(self):
-        cursor = self.conn.cursor()
-        sql = """
-        SELECT scenario_id, text FROM scenario_response
-        """
-        cursor.execute(sql)
+        return self.query_embedding_dict, response_cluster_dict
 
-        response_class, scenario_response = [], []
-        for id, text in cursor:
-            response_class.append(id)
-            scenario_response.append(text)
-        cursor.close()
-        return response_class, scenario_response
-
-    def _get_query(self):
-        cursor = self.conn.cursor()
-        sql = """
-        SELECT scenario_id, text FROM scenario_query
-        """
-        cursor.execute(sql)
+    def _get_query_response(self):
+        query_response = ScenarioDao.get_entire_scenario_query_response()
 
         query_class, scenario_query = [], []
-        for id, text in cursor:
-            query_class.append(id)
-            scenario_query.append(text)
-        cursor.close()
-        return query_class, scenario_query
+        for res in query_response[0]:
+            query_class.append(res['scenario_id'])
+            scenario_query.append(res['text'])
 
-
-class DB_connection_info:
-    host = '52.141.21.117'
-    port = 3306
-    user = 'root'
-    passwd = 'piglet'
-    db = 'tikitaka'
-    charset = 'utf8'
+        response_class, scenario_response = [], []
+        for res in query_response[1]:
+            response_class.append(res['scenario_id'])
+            scenario_response.append(res['text'])
+        return (query_class, scenario_query), (response_class, scenario_response)
 
 
 def construct_ques_embed_dict(querys: list, cateogry: list, inferencer):
