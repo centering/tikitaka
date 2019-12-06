229aa6ed605b1d125435666df0ae0e0b614f47c7
MJ-Jang <xkxpa@korea.ac.kr>
2019-12-06 15:34:56 +0900

    connect blacklist filtering

--- talkengine/data_util.py
+++ talkengine/data_util.py
@@ -23,6 +23,9 @@ ReactionDao = mod_react.ReactionDao
 mod_set = importlib.import_module('model.setting_dao')
 SettingDao = mod_set.SettingDao
 
+mod_black = importlib.import_module('model.blacklist_dao')
+BlacklistDao = mod_black.BlacklistDao
+
 
 class DataController(metaclass=abc.ABCMeta):
     @abc.abstractmethod
@@ -164,6 +167,64 @@ class ReactionDataController(DataController):
         response_class, reaction_response = [], []
         for res in query_response[1]:
             response_class.append(res['reaction_id'])
+            reaction_response.append(res['text'])
+        return (query_class, reaction_query), (response_class, reaction_response)
+
+    def _get_threshold(self):
+        output = {}
+        for res in SettingDao.get_setting_list():
+            output[res['name']] = res['value']
+        return output
+
+
+class SlangDataController(DataController):
+    def __init__(self):
+        self.init_setup()
+
+    def init_setup(self):
+        (query_class, self.querys), (res_class, self.responses) = self._get_query_response()
+        self.threshold_dict = self._get_threshold()
+
+        self.response_cluster_dict = construct_cluster_dict(self.responses, res_class)
+        self.query_cluster_dict = construct_sent_dict(self.querys, query_class)
+        return self.query_cluster_dict, self.response_cluster_dict
+
+    def update(self):
+        query_embedding_dict, response_cluster_dict = self._update_query_response()
+        self.threshold_dict = self._get_threshold()
+        return query_embedding_dict, response_cluster_dict
+
+    def _update_query_response(self):
+        self.threshold_dict = self._get_threshold()
+        (query_class, querys), (response_class, responses) = self._get_query_response()
+
+        # update query
+        if querys == self.querys:
+            query_cluster_dict = self.query_cluster_dict
+        else:
+            query_cluster_dict = construct_sent_dict(responses, response_class)
+            self.querys = querys
+
+        # update response
+        if responses == self.responses:
+            response_cluster_dict = self.response_cluster_dict
+        else:
+            response_cluster_dict = construct_cluster_dict(responses, response_class)
+            self.responses = responses
+
+        return query_cluster_dict, response_cluster_dict
+
+    def _get_query_response(self):
+        query_response = BlacklistDao.get_entire_blacklist_query_response()
+
+        query_class, reaction_query = [], []
+        for res in query_response[0]:
+            query_class.append(res['blacklist_id'])
+            reaction_query.append(normalize_text(res['text']))
+
+        response_class, reaction_response = [], []
+        for res in query_response[1]:
+            response_class.append(res['blacklist_id'])
             reaction_response.append(res['text'])
         return (query_class, reaction_query), (response_class, reaction_response)
 
